ab7bf3ef524a8e0f802e35e31ef0ae7c965f4452
seacat271 <seacat271083@gmail.com>
2022-08-04 21:19:52 +0300

    done task#2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listIngredientsContainerEl = document.querySelector("#ingredients");
+
+const createListIngredients = (array) => {
+return array.map(ingredient => {
+  const itemEl = document.createElement("li");
+  itemEl.classList.add("item");
+  itemEl.textContent = ingredient;
+  return itemEl; 
+})
+};
+
+const listIngredientsEl = createListIngredients(ingredients);
+listIngredientsContainerEl.append(...listIngredientsEl);
+
+
