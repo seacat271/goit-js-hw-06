0b23ebe749f5f9ac72cef9f0ca6b3901c0761c53
seacat271 <seacat271083@gmail.com>
2022-08-05 20:04:55 +0300

    done task#8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,24 @@
+const loginForm = document.querySelector(".login-form")
 
+function HandleSubmitLoginForm(event) {
+    event.preventDefault();
+
+    const {
+        elements: { email, password }
+      } = event.currentTarget;
+
+      if (email.value === "" || password.value === "") {
+        return alert("Будь-ласка, заповніть всі поля");
+      }
+
+    const dataInputObj = {
+        email : email.value,
+        password : password.value
+}
+
+console.log(dataInputObj);
+
+event.currentTarget.reset();
+}
+
+loginForm.addEventListener("submit", HandleSubmitLoginForm)
