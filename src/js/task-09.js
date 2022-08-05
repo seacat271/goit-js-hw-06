0c90bbeb3846ba42b7e37170e5add5c0b37fe9d3
seacat271 <seacat271083@gmail.com>
2022-08-05 20:30:41 +0300

    done task#9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const widgetBtn = document.querySelector(".change-color");
+
+const widgetDescriptionColor = document.querySelector(".color");
+
+widgetBtn.addEventListener("click", changeColor);
+
+function changeColor() {
+  const color = getRandomHexColor();
+ widgetDescriptionColor.textContent = color
+ document.body.style.backgroundColor = color
+}
